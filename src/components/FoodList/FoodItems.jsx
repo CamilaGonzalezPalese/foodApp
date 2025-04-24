c098a72ddacc84606c7d8666125976484ce15f98
CamilaGonzalezPalese <62571300+CamilaGonzalezPalese@users.noreply.github.com>
2025-04-24 17:27:02 -0300

    FoodList en dos componentes

---
+++ src/components/FoodList/FoodItems.jsx
@@ -0,0 +1,18 @@
+import './FoodList.css';
+
+function FoodItems({ food }) {
+  return (
+    <div className={`food-item ${food.stock === 0 ? 'no-stock' : ''}`}>
+      <figure>
+        <span className="emoji">{food.emoji}</span>
+      </figure>
+      <div className='info-product'>
+        <button className={food.stock === 0 ? 'out' : ''}>
+          <p className='price'>${food.stock}</p>
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default FoodItem;
--- src/components/FoodList/FoodList.jsx
+++ src/components/FoodList/FoodList.jsx
@@ -1,24 +1,15 @@
-import {foods} from "/src/assets/Foods.js"
-import './FoodList.css'
-function FoodList() {
+import { foods } from '/src/assets/Foods.js';
+import FoodItems from './FoodItems';
+import './FoodList.css';
 
+function FoodList() {
   return (
     <div className='food-list'>
-    {foods.map(food => (
-      <div className={`food-item ${food.stock === 0 ? 'no-stock' : ''}`} key={food.id}>
-        <figure>
-          <span className="emoji">{food.emoji}</span>
-        </figure>
-        <div className='info-product'>
-          <button className={food.stock === 0 ? 'out' : ''}>
-          <p className='price'>${food.stock}</p>
-          </button>
-        </div>
-      </div>
-    ))}
-  </div>
+      {foods.map((food) => (
+        <FoodItems key={food.id} food={food} />
+      ))}
+    </div>
   );
-
 }
 
 export default FoodList;
